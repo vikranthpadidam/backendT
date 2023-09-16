4b82ca651696de6a4f734ba4821caf623d0a8363
vikranthpadidam <vikranthpadidam@gmail.com>
2023-09-16 22:52:11 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,12 @@
+// Connecting to moongose
+const mongoose=require("mongoose");
+
+const mongoURL="mongodb+srv://vikranthpadidam:iYF0p9er7ithjX5y@cluster0.i7qbox7.mongodb.net/TMShare?retryWrites=true&w=majority";
+
+const connecttoMongo=()=>{
+    mongoose.connect(mongoURL,()=>{
+        console.log("Successfully connected to mongoDB");
+    })
+}
+
+module.exports=connecttoMongo;
